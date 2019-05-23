e3516a7dc8513d5af7e4fb36d4d678b1d016a710
John Wang <johnjinghuawang@gmail.com>
2019-05-22 20:40:28 -0700

    Fix Bugs

--- scripts/chart1.R
+++ scripts/chart1.R
@@ -3,22 +3,14 @@ library("dplyr")
 library("ggplot2")
 library("leaflet")
 
-airbnb <- read.csv("../data/seattle-airbnb/listings.csv", stringsAsFactors = FALSE)
+df <- read.csv("../data/seattle-airbnb/listings.csv", stringsAsFactors = FALSE)
 
 chart1 <- function(df) {
   # Select the columns of interest
   df <- df %>% 
     select(latitude, longitude, name, review_scores_rating, listing_url)
   
-  #create descriptions  
-  df <- df %>% 
-    mutate(
-      details = paste0(
-        "Name: ", name, "</br>",
-        "Rating: ", review_scores_rating, "</br>",
-        "[Open in Airbnb](", listing_url, ")")
-    )
-  map <- leaflet() %>%
+  map <- leaflet(df) %>%
     addProviderTiles("CartoDB.Positron") %>%
     setView(lng = -122.3321, lat = 47.6062, zoom = 10) %>% 
     addCircles(
@@ -38,4 +30,4 @@ chart1 <- function(df) {
   map
 }
 
-map <- chart1(airbnb)
+map <- chart1(df)
--- scripts/chart2.R
+++ scripts/chart2.R
@@ -7,7 +7,14 @@ df <- read.csv("../data/seattle-airbnb/listings.csv", stringsAsFactors = FALSE)
 
 # violent chart of the price using number of bedrooms as factor
 chart2 <- function(df) {
-  price_by_bedrooms <- ggplot(df,  aes(x = bedrooms, y=price)) + 
-    geom_violin(trim = FALSE, aes(fill = factor(bedrooms))) 
+  df <- df %>% 
+    mutate(Price = as.numeric(gsub("[\\$,]", "", price)))
+  
+  price_by_bedrooms <- ggplot(df,  aes(x = bedrooms, y=Price)) + 
+    geom_violin(trim = FALSE, 
+                aes(fill = factor(bedrooms)), 
+                draw_quantiles = c(0.5))
   return(price_by_bedrooms)
 }
+
+chart2(df)
