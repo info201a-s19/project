5b7e3c8a7ed349819c22ef353857c727249fd04a
Annie Baby <Anniebaby@AnniedeMacBook-Air-2.local>
2019-05-24 12:33:23 -0700

    delete top airbnb table function

--- scripts/tables.R
+++ scripts/tables.R
@@ -9,19 +9,6 @@ markdown_image <- function(link, alt) {
   paste0("![", alt, "](", link, ")")
 }
 
-top_airbnb_table <- function(df) {
-  df <- arrange(df, desc(review_scores_rating)) %>%
-    head(20) %>%
-    mutate(picture = markdown_image(picture_url,
-                                    paste0(name, "'s picture"))) %>%
-    mutate(name = markdown_link(name, listing_url)) %>%
-    mutate(host = markdown_link(host_name, host_url)) %>%
-    select(picture, name, room_type, host, street,
-           review_scores_rating)
-  kable(df, col.names = c("Picture", "Name", "Room Type", "Host",
-                          "Location", "Review Score"))
-}
-
 summary_by_type_table <- function(df) {
   df <- df %>%
     group_by(room_type) %>%
