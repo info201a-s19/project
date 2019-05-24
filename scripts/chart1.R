17bb5183614acb33095b8d996332e57a8d75c9e5
John Wang <johnjinghuawang@gmail.com>
2019-05-23 20:37:52 -0700

    Fix bug

--- scripts/chart1.R
+++ scripts/chart1.R
@@ -5,7 +5,6 @@ library("leaflet")
 
 # Plot all the listing on a map and put some useful information 
 # in the popup window 
-
 chart1 <- function(df) {
   map <- leaflet(df) %>%
     addProviderTiles("CartoDB.Positron") %>%
@@ -15,7 +14,7 @@ chart1 <- function(df) {
       lng = ~ longitude,
       popup = ~ paste(
         paste0("<b><img src=", thumbnail_url, ">"),
-        paste0("Name: ", name),
+        name,
         paste0("Rating: ", review_scores_rating),
         paste0("<b><a href=", listing_url, ">Open in Airbnb</a></b>"),
         sep = "<br/>"
